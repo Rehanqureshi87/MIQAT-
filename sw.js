27d0cc6c7e39637a5dd8e8191e1412d688de4557
Rehanqureshi87 <uddinqureshir@gmail.com>
2026-07-17 10:22:16 +0530

    Update service worker to include additional assets

--- sw.js
+++ sw.js
@@ -1,12 +1,24 @@
 const CACHE = 'miqat-v1';
-const ASSETS = ['/', '/index.html'];
+const ASSETS = [
+  '/MIQAT-/',
+  '/MIQAT-/index.html',
+  '/MIQAT-/manifest.json'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request)
+      .then(r => r || fetch(e.request))
   );
 });
